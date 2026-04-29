f28e2b5f539e7e99b50c8c58b8a407966e8f6002
Bren1102 <bmorrone@hotmail.com>
2026-04-29 09:05:36 -0300

    correcion idioma

--- mascotas/models.py
+++ mascotas/models.py
@@ -2,34 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Mascota(models.Model):
-    name = models.CharField(max_length=100)
-    species = models.CharField(max_length=100) # Perro, Gato, etc.
-    breed = models.CharField(max_length=100)   # Raza
-    age = models.IntegerField()
-    weight = models.FloatField()
-    furType = models.CharField(max_length=100) # Tipo de pelo
-    furColor = models.CharField(max_length=100) # Color de pelo
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mascotas')
+    nombre = models.CharField(max_length=100)
+    especie = models.CharField(max_length=100) # Perro, Gato, etc.
+    raza = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    peso = models.FloatField()
+    tipo_pelo = models.CharField(max_length=100)
+    color_pelo = models.CharField(max_length=100)
+    dueño = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mascotas')
     
     def __str__(self):
-        return f"{self.name} ({self.species})"
+        return f"{self.nombre} ({self.especie})"
 
 class Veterinaria(models.Model):
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.name
+        return self.nombre
 
 class Turno(models.Model):
-    pet = models.ForeignKey(Mascota, on_delete=models.CASCADE, related_name='turnos')
-    veterinary = models.ForeignKey(Veterinaria, on_delete=models.CASCADE, related_name='turnos')
-    dateTime = models.DateTimeField()
-    reason = models.CharField(max_length=255)
+    mascota = models.ForeignKey(Mascota, on_delete=models.CASCADE, related_name='turnos')
+    veterinaria = models.ForeignKey(Veterinaria, on_delete=models.CASCADE, related_name='turnos')
+    fecha_hora = models.DateTimeField()
+    motivo = models.CharField(max_length=255)
     
     class Meta:
-        unique_together = ['veterinary', 'dateTime']
+        unique_together = ['veterinaria', 'fecha_hora']
 
     def __str__(self):
-        return f"Turno: {self.pet.name} en {self.veterinary.name}"
+        return f"Turno: {self.mascota.nombre} en {self.veterinaria.nombre}"
